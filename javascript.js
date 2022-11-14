71a425a41e881047383dcf5e0a0a893907a535c3
wuaann <114000299+wuaann@users.noreply.github.com>
2022-11-14 21:16:23 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+document.getElementById('banner_left').addEventListener("mouseover",function (){
+    document.getElementById('bg_img').style.backgroundImage = "url(img/img_left.png)"
+},false)
+
+document.getElementById('banner_middle').addEventListener("mouseover",function (){
+    document.getElementById('bg_img').style.backgroundImage = "url(img/img_middle.png)"
+},false)
+
+document.getElementById('banner_right').addEventListener("mouseover",function (){
+    document.getElementById('bg_img').style.backgroundImage = "url(img/img_right.png)"
+},false)
